3ffa77908cd155e9910cad0090cf37ba7dc2e784
Rafael Xavier de Souza <rxaviers@gmail.com>
2015-04-20 15:52:00 -0300

    Consistently use `"` instead of `'`

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-var Globalize = require('globalize');
+var Globalize = require("globalize");
 var mixins = {};
 
 Object.getOwnPropertyNames(Globalize).forEach(function(fn) {
     if (fn.indexOf("format") === 0) {
         var fnString = Globalize[fn].toString();
-        var argString = fnString.substr(fnString.indexOf('(')+1, fnString.indexOf(')')-(fnString.indexOf('(')+1)).trim();
-        var argArray = argString.split(', ');
+        var argString = fnString.substr(fnString.indexOf("(")+1, fnString.indexOf(")")-(fnString.indexOf("(")+1)).trim();
+        var argArray = argString.split(", ");
 
         (function(currentFn, currentArgs) {
             var formatter = function(nextProps) {
                 var componentProps = nextProps || this.props;
                 var propArgs = currentArgs.map(function(element) {
-                        return componentProps[element.replace(/(\s\/\*|\*\/)/,'').trim()];
+                        return componentProps[element.replace(/(\s\/\*|\*\/)/,"").trim()];
                     });
 
                 Globalize.locale( componentProps["locale"] );
