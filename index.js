5e2c783ce060ba4382816846115d90438f29f42f
Rafael Xavier de Souza <rxaviers@gmail.com>
2015-07-08 09:45:46 -0300

    Index: Support minified Globalize
    Fixes #10

--- index.js
+++ index.js
@@ -1,38 +1,42 @@
 var Globalize = require("globalize");
 var React = require("react");
+
+var fns = {
+    formatCurrency: ["value", "currency", "options"],
+    formatDate: ["value", "options"],
+    formatMessage: ["key", "variables"],
+    formatNumber: ["value", "options"],
+    formatRelativeTime: ["value", "unit", "options"]
+};
 var ReactGlobalize = {};
 
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-Object.getOwnPropertyNames(Globalize).forEach(function(fn) {
-    if (fn.indexOf("format") === 0) {
-        var Fn = capitalizeFirstLetter(fn);
-        var fnString = Globalize[fn].toString();
-        var argString = fnString.substr(fnString.indexOf("(")+1, fnString.indexOf(")")-(fnString.indexOf("(")+1)).trim();
-        var argArray = argString.split(", ");
+Object.keys(fns).forEach(function(fn) {
+    var Fn = capitalizeFirstLetter(fn);
+    var argArray = fns[fn];
 
-        ReactGlobalize[Fn] = React.createClass({
-            displayName: Fn,
-            render: function() {
-                var formatted;
-                var componentProps = this.props;
-                var instance = Globalize;
-                var propArgs = argArray.map(function(element) {
-                    return componentProps[element.replace(/(\s\/\*|\*\/)/,"").trim()];
-                });
+    ReactGlobalize[Fn] = React.createClass({
+        displayName: Fn,
+        render: function() {
+            var formatted;
+            var componentProps = this.props;
+            var instance = Globalize;
+            var propArgs = argArray.map(function(element) {
+                return componentProps[element];
+            });
 
-                if (componentProps["locale"]) {
-                  instance = Globalize(componentProps["locale"]);
-                }
-
-                formatted = instance[fn].apply(instance, propArgs);
-
-                return React.DOM.span(null, formatted);
+            if (componentProps["locale"]) {
+                instance = Globalize(componentProps["locale"]);
             }
-        });
-    }
+
+            formatted = instance[fn].apply(instance, propArgs);
+
+            return React.DOM.span(null, formatted);
+        }
+    });
 });
 
 module.exports = ReactGlobalize;
