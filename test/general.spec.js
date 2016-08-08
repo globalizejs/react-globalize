806b8d44142730fc593db7a1280c86b561001ac0
Rafael Xavier de Souza <rxaviers@gmail.com>
2016-08-08 16:45:26 -0300

    Don't forward all props to underlying dom component
    Fixes #57

--- test/general.spec.js
+++ test/general.spec.js
@@ -1,7 +1,18 @@
 /*global expect React shallow Globalize*/
 import FormatCurrency from "../dist/currency";
+import FormatMessage from "../dist/message";
 
 describe("Any Component", () => {
+    it("doesn't forward ReactGlobalize specific props to underlying DOM component", () => {
+        let wrapper = shallow(<FormatCurrency locale="de" currency="EUR">{150}</FormatCurrency>);
+        expect(wrapper.props()).to.contain.all.keys(["children"]);
+        expect(wrapper.props()).to.not.contain.any.keys(["locale", "currency"]);
+
+        wrapper = shallow(<FormatMessage className="a-class-name" style={{aStyle: 1}} elements={{foo: 1}} variables={{bar: 2}}>Hello</FormatMessage>);
+        expect(wrapper.props()).to.contain.all.keys(["children", "className", "style"]);
+        expect(wrapper.props()).to.not.contain.any.keys(["elements", "variables"]);
+    });
+
     it("overrides default locale to format 150 as 150,00 €", () => {
         const wrapper = shallow(<FormatCurrency locale="de" currency="EUR">{150}</FormatCurrency>);
         expect(wrapper.text()).to.equal("150,00 €");
